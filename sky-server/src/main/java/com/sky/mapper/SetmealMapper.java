0dbc306bf5ce306a161e7d175bf69ec17757a614
mark <2483178594@qq.com>
2025-09-20 16:59:12 +0800

    控制台显示

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -12,6 +12,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
@@ -84,4 +85,12 @@ public interface SetmealMapper {
      */
     @AutoFill(OperationType.UPDATE)
     void update_2(Setmeal setmeal);
+
+
+    /**
+     * 根据条件统计套餐数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
